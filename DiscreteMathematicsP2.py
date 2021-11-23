b1d11c5f4c9149da9b776970562dbb1576f1886a
DarioCosta <dario.c.alves@hotmail.com>
2021-11-23 19:08:17 +0000

    Created Exercise a

--- DiscreteMathematicsP2.py
+++ DiscreteMathematicsP2.py
@@ -1,6 +1,26 @@
 #Imports
 import numpy as np
 
+def solveEquation(v0, g, t):
+    """
+
+    This function will calculate the mathematical equation 
+    that represents an object in vertical movement.
+
+    y(t) = v0t-(1/2)gt**2
+
+    y-height(position) as a function of time t
+    v0-initial speed at t=0
+    g-gravity acceleration
+    t-time
+
+    """
+    result = v0*t-(1/2)*g*t**2
+    return print("Result of the equation: ", np.round(result,decimals=2))
+
+class negativeValue(Exception):
+    pass
+
 #Creating 'menu' array
 menu = {}
 
@@ -20,7 +40,7 @@ while True:
         print(entry, menu[entry])
 
     #Save user selected option in variable 'selected'
-    selected=input("Please, select the exercise: ")
+    selected=input("\nPlease, select the exercise: ")
 
     """
     When the user input from [a-c] it will goes to the respective exercise
@@ -30,9 +50,24 @@ while True:
 
     #Exercise a - Mathematical Equation
     if selected == 'a':
-        print("Exercise a - Mathematical Equation")
+        print("\nExercise a - Mathematical Equation")
         print("---------------------------------------------")
-    
+        while True:
+
+            try:
+                v0 = float(input("Insert initial speed(v0): "))
+                g = float(input("Insert gravity acceleration(g): "))
+                t = float(input("Insert time(t): "))
+                                
+                if v0 < 0 or g < 0 or t < 0:
+                    raise ValueError
+
+            except ValueError:            
+                print("Please, insert only positive numbers.")
+            else:
+                solveEquation(v0,g,t)
+                break 
+                
     elif selected == 'b':
         print("Exercise b - 2D Graph")
         print("---------------------------------------------")
@@ -44,6 +79,6 @@ while True:
     elif selected == 'd':
         break
     
-    else:
+    else: 
         print("\nInvalid Option Selected!")
 
