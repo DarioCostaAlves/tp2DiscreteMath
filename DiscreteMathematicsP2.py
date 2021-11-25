07b8eaa5a4eda98dd49e6f0f5ab467548d4ea182
João Freitas <joaofreitas@joaofreitas.Home>
2021-11-25 00:48:51 +0000

    Updates in previous exercise & Creating Exercise b (Need Updates)

--- DiscreteMathematicsP2.py
+++ DiscreteMathematicsP2.py
@@ -1,9 +1,9 @@
 #Imports
 import numpy as np
+import matplotlib.pyplot as plt
 
 def solveEquation(v0, g, t):
     """
-
     This function will calculate the mathematical equation 
     that represents an object in vertical movement.
 
@@ -13,14 +13,39 @@ def solveEquation(v0, g, t):
     v0-initial speed at t=0
     g-gravity acceleration
     t-time
-
     """
-    result = v0*t-(1/2)*g*t**2
-    return print("Result of the equation: ", np.round(result,decimals=2))
 
-class negativeValue(Exception):
-    pass
+    y = v0*t-(1/2)*g*t**2
+    return print("Result of the equation: ", np.round(y,decimals=2))
 
+def build2DGraph(v0,g,it,ft):
+    
+    """
+    This function will generate the 2D graph that represents
+    the variation of y in a time interval(t)
+    """
+
+    #Time Interval -> [Initial Time - Final Time]
+    ti = np.linspace(it,ft)
+    
+    #y(t) - free fall equation
+    y = v0*ti-(1/2)*g*ti**2
+    
+    #Creating plot
+    fig, ax = plt.subplots()
+    lines = plt.plot(ti,y)
+
+    #Naming title and labels
+    ax.set_title("Variation of y in a Time Interval")
+    ax.legend(("Height","dawd"))
+    ax.set_xlabel("Time interval(t)")
+    ax.set_ylabel("Height(y)")
+    
+    #show the plot
+    plt.show()
+
+
+    
 #Creating 'menu' array
 menu = {}
 
@@ -58,7 +83,7 @@ while True:
                 v0 = float(input("Insert initial speed(v0): "))
                 g = float(input("Insert gravity acceleration(g): "))
                 t = float(input("Insert time(t): "))
-                                
+
                 if v0 < 0 or g < 0 or t < 0:
                     raise ValueError
 
@@ -69,9 +94,26 @@ while True:
                 break 
                 
     elif selected == 'b':
-        print("Exercise b - 2D Graph")
+        print("\nExercise b - 2D Graph")
         print("---------------------------------------------")
-    
+        
+        while True:
+
+            try:
+                v0 = float(input("Insert initial speed(v0): "))
+                g = float(input("Insert gravity acceleration(g): "))
+                initialTime = float(input("Insert initial time interval(t): "))
+                finalTime = float(input("Insert final time interval(t): "))
+
+                if v0 < 0 or g < 0 or finalTime < 0:
+                    raise ValueError
+
+            except ValueError:
+                print("Please, insert only positive numbers.")
+            else:
+                build2DGraph(v0,g,initialTime,finalTime)
+                break
+
     elif selected == 'c':
         print("Exercise c - 3D Graph")
         print("---------------------------------------------")
